238a8928afea576bbf78e47e4083464a3393370d
jassmunday <129063092+jassmunday@users.noreply.github.com>
2023-11-29 22:59:16 +0530

    first commit

---
+++ components/navbar.tsx
@@ -0,0 +1,43 @@
+import { UserButton, auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import StoreSwitcher from "@/components/store-switcher";
+import { MainNav } from "@/components/main-nav";
+import { ThemeToggle } from "@/components/theme-toggle";
+import prismadb from "@/lib/prismadb";
+import dynamic from "next/dynamic";
+
+const Navbar = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect('/sign-in');
+  }
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    }
+  });
+
+  return ( 
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
+        <StoreSwitcher items={stores} />
+        <MainNav className="mx-6" />
+        <div className="ml-auto flex items-center space-x-4">
+          <ThemeToggle />
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      </div>
+    </div>
+  );
+};
+ 
+
+
+
+export default dynamic (() => Promise.resolve(Navbar), {ssr: false});
+//import dynamic from "next/dynamic";
+//The below and above statement to avoid hydration error while rendering to the server.
+//export default Navbar;
